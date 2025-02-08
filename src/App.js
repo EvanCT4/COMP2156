6c6755efb08c2e3710091db2155646594213d59b
Evan <evanct48@gmail.com>
2025-02-08 10:28:10 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>student ID: 101256897</h1>
+      <h1>student Name: Evan Thomas</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developed By: Pritesh Patel
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
